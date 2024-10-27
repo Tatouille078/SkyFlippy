b76ea92e608d2902bb2cc6f1c34daab43ded80c3
Tatouille078 <tatouille.loick@gmail.com>
2024-10-27 21:47:56 +0100

    Bg Changes and added parallax

--- src/background.tsx
+++ src/background.tsx
@@ -1,54 +1,99 @@
 import { useRef } from 'react';
 
 const AnimatedShapes = () => {
-  const shapes = useRef<Array<{ 
+  const fgShapes = useRef<Array<{ 
     left: string; 
     top: string; 
-    width: string; 
-    height: string; 
+    width: number; 
+    height: number; 
     opacity: number; 
     animationDelay: string; 
     animationDuration: string;
+    rotateStart: string;
     className: string;
-    rotateDirection: string;
-    translateDirection: string;
-    rotateAmount: number;
   }>>([]);
 
-  if (shapes.current.length === 0) {
-    shapes.current = [...Array(30)].map(() => ({
+  const bgShapes = useRef<Array<{ 
+    left: string; 
+    top: string; 
+    width: number; 
+    height: number; 
+    opacity: number; 
+    animationDelay: string; 
+    animationDuration: string;
+    rotateStart: string;
+    className: string;
+  }>>([]);
+
+  if (fgShapes.current.length === 0) {
+    fgShapes.current = [...Array(20)].map(() => ({
       left: `${Math.random() * 100}%`,
       top: `${Math.random() * 140 + 100}%`,
-      width: `${Math.random() * 130 + 70}px`,
-      height: `${Math.random() * 130 + 70}px`,
-      opacity: 0.3,
-      animationDelay: `${Math.random() * 5 + 1}s`,
-      animationDuration: `${Math.random() * 15 + 15}s`,
+      width: Math.random() * 90 + 110,
+      height: Math.random() * 90 + 110,
+      opacity: 0.4,
+      animationDelay: `${Math.random() * 5}s`,
+      animationDuration: `${Math.random() * 15 + 20}s`,
+      rotateStart: `${Math.random() * 360}`,
       className: `absolute ${
-        Math.random() > 0.5 ? 'rounded-full' : ''
+        Math.random() > 0.5 ? 'rounded-full' : 'rounded-lg'
       } ${
         Math.random() > 0.5 
           ? Math.random() > 0.5 ? 'border-8 border-pink-200' : 'border-8 border-purple-400' 
-          : Math.random() > 0.5 ? 'bg-purple-200 rounded-md' : 'bg-pink-400 rounded-full'
+          : Math.random() > 0.5 ? 'bg-purple-200' : 'bg-pink-400'
       }`,
-      rotateDirection: Math.random() > 0.5 ? '' : '-',
-      translateDirection: Math.random() > 0.5 ? '' : '-',
-      rotateAmount: Math.random() * 720,
     }));
   }
+  if (bgShapes.current.length === 0) {
+    bgShapes.current = [...Array(20)].map(() => ({
+      left: `${Math.random() * 80 + 10}%`,
+      top: `${Math.random() * 140 + 120}%`,
+      width: Math.random() * 30 + 50,
+      height: Math.random() * 30 + 50,
+      opacity: 0.2,
+      animationDelay: `${Math.random() * 5}s`,
+      animationDuration: `${Math.random() * 15 + 50}s`,
+      rotateStart: `${Math.random() * 360 + 360}`,
+      className: `absolute ${
+        Math.random() > 0.5 ? 'rounded-full' : 'rounded-md'
+      } ${
+        Math.random() > 0.5 
+          ? Math.random() > 0.5 ? 'border-4 border-pink-200' : 'border-4 border-purple-400' 
+          : Math.random() > 0.5 ? 'bg-purple-200' : 'bg-pink-400'
+      }`,
+    }));
+  }
+
+  const translateDirection = Math.random() > 0.5 ? '' : '-';
+  const rotateAmount = Math.random() * 720 + 360;
 
   return (
     <div className="top-0 left-0 right-0 bottom-0 fixed inset-0 pointer-events-none overflow-hidden">
       <div className="absolute inset-0">
-        {shapes.current.map((shape, i) => (
+        {fgShapes.current.map((shape, i) => (
           <div
             key={i}
             className={shape.className}
             style={{
               left: shape.left,
               top: shape.top,
-              width: shape.width,
-              height: shape.height,
+              width: `${shape.width}px`,
+              height: `${shape.height}px`,
+              opacity: shape.opacity,
+              animation: `float ${shape.animationDuration} linear infinite`,
+              animationDelay: shape.animationDelay,
+            }}
+          />
+        ))}
+        {bgShapes.current.map((shape, i) => (
+          <div
+            key={i}
+            className={shape.className}
+            style={{
+              left: shape.left,
+              top: shape.top,
+              width: `${shape.width}px`,
+              height: `${shape.height}px`,
               opacity: shape.opacity,
               animation: `float ${shape.animationDuration} linear infinite`,
               animationDelay: shape.animationDelay,
@@ -62,7 +107,7 @@ const AnimatedShapes = () => {
             transform: translate(0, 0) rotate(0deg);
           }
           100% {
-            transform: translate(${shapes.current[0].translateDirection}5vw, -240vh) rotate(${shapes.current[0].rotateAmount}deg);
+            transform: translate(${translateDirection}5vw, -240vh) rotate(${rotateAmount}deg);
           }
         }
       `}</style>
