9c5fc18615955de40da9e29ceec66cfeecf6ae11
Tatouille078 <tatouille.loick@gmail.com>
2025-02-09 22:59:21 +0100

    v1.0.2 deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "./index.css"
 import { ContextProvider } from './context.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { TranslationProvider } from './contexts/TranslationContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <TranslationProvider>
       <ContextProvider>
         <App />
       </ContextProvider>
     </TranslationProvider>
-  </BrowserRouter>
+  </HashRouter>
 )
